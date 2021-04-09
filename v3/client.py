405064be53cfef802b33af3bb8a3dc5c8eedc91b
Ha-sake <jm-almeida@live.com.pt>
2021-04-09 03:43:36 +0100

    Set up POST/GET/DELETE/PUT for URL /utilizadores Refined progress table Added some client-side tests

--- v3/client.py
+++ v3/client.py
@@ -8,10 +8,26 @@ import json
 
 #### TEST LINES - PROOF OF CONCEPT
 
-r = requests.get('http://localhost:5000/utilizadores')
+# utilizador = {'nome': "João Café", 'senha': "AAAAAAAAAAAAA"}
+# r = requests.post('http://localhost:5000/utilizadores', json = utilizador)
+# print(r.status_code)
+# print('***')
+
+####
+#
+r = requests.delete('http://localhost:5000/utilizadores')
 print(r.status_code)
-print(r.json())
-print(r.headers)
 print('***')
 
 ####
+
+# r = requests.get('http://localhost:5000/utilizadores')
+# print(r.status_code)
+# print('***')
+
+####
+
+# utilizador = {'senha': 'TESTE123123123'}
+# r = requests.put('http://localhost:5000/utilizadores/5', json = utilizador)
+# print(r.status_code)
+# print('***')
--- v3/server.py
+++ v3/server.py
@@ -3,40 +3,139 @@ import sqlite3
 from db import connect_db
 from os.path import isfile
 from flask import Flask, g, request, make_response, jsonify
+import traceback as t
 
 app = Flask(__name__)
 DATABASE = "data.db"
 
 # Routes...
 
-@app.route('/utilizadores', methods = ['POST', 'PUT', 'DELETE', 'GET'])
-@app.route('/utilizadores/<int:id_user>', methods = ["GET"])
+@app.route('/utilizadores', methods = ['POST', 'GET', 'DELETE'])
+@app.route('/utilizadores/<int:id_user>', methods = ["GET", "DELETE", "PUT"])
 # Check reference server for individual functions and arguments...
 def utilizadores(id_user = None):
 
     r = make_response()
 
     if request.method == "GET":
-        # Logic if GET -> READ or READ ALL
-        if id_user:
-            sql = f"SELECT id, sigla, designacao FROM avaliacoes WHERE id = {id_user}"
-            results = query_db(sql)
-            print(results)
-            r = make_response(jsonify(results))
-            # TODO: This may not always be true, sort it out later
-            r.status_code = 200
+        try:
+            # Logic if GET -> READ
+            if id_user:
+                sql = f"SELECT id, nome FROM utilizadores WHERE id = {id_user}"
+                results = query_db(sql)
+
+                r = make_response(jsonify(results))
+
+                if len(results) > 0:
+                    r.status_code = 200
+                else:
+                    r.status_code = 400
+            else:
+                sql = f"SELECT id, nome FROM utilizadores"
+                results = query_db(sql)
+
+                r = make_response(jsonify(results))
+
+                if len(results) > 0:
+                    r.status_code = 200
+                else:
+                    r.status_code = 400
+
+        except:
+            r = make_response()
+            r.status_code = 500
+
 
     elif request.method == "POST":
         # Logic if POST -> CREATE
-        pass
+
+        try:
+
+            data = request.json
+
+            name = data["nome"]
+            password = data["senha"]
+
+            sql = f"INSERT INTO utilizadores (nome, senha) VALUES ('{name}', '{password}')"
+
+            query_db(sql)
+
+            r.status_code = 200
+
+        except TypeError:
+            r.status_code = 500
+
+
 
     elif request.method == "DELETE":
         # Logic if DELETE -> DELETE
-        pass
+
+        try:
+            if id_user:
+                # Checking for results to decide response status code since SQLite DELETE
+                # does not return number of deleted rows.
+                sql = f"SELECT * FROM utilizadores WHERE id = {id_user};"
+                results = query_db(sql)
+
+                # Deleting corresponding rows.
+                sql = f"DELETE FROM utilizadores WHERE id = {id_user}"
+                query_db(sql)
+
+                if len(results) > 0:
+                    r.status_code = 200
+                else:
+                    r.status_code = 400
+
+            else:
+                # Checking for results to decide response status code since SQLite DELETE
+                # does not return number of deleted rows.
+
+                sql = f"SELECT * FROM utilizadores"
+                results = query_db(sql)
+
+                sql = f"DELETE FROM utilizadores"
+
+                query_db(sql)
+
+                if len(results) > 0:
+                    r.status_code = 200
+                else:
+                    r.status_code = 400
+
+
+
+
+        except:
+            # If no rows were updated, set status code 400.
+            r.status_code = 500
+
 
     elif request.method == "PUT":
         # Logic if PUT/PATCH -> UPDATE
-        pass
+        try:
+            data = request.json
+            password = data["senha"]
+
+            # Checking for results to decide response status code since SQLite UPDATE
+            # does not return number of deleted rows.
+            sql = f"SELECT * FROM utilizadores WHERE id = {id_user};"
+            results = query_db(sql)
+
+            # Updating corresponding rows.
+            sql = f"UPDATE utilizadores SET senha = '{password}' WHERE id = {id_user};"
+            query_db(sql)
+
+            if len(results) > 0:
+                r.status_code = 200
+            else:
+                # If no rows were updated, set status code 400.
+                r.status_code = 400
+
+        except:
+            r.status_code = 500
+
+
+
 
     return r
 
@@ -97,7 +196,9 @@ def get_db():
     return db
 
 def query_db(query, args=(), one=False):
-    cursor = get_db().execute(query, args)
+    conn = get_db()
+    cursor = conn.execute(query, args)
+    conn.commit()
     rv = cursor.fetchall()
     cursor.close()
     return (rv[0] if rv else None) if one else rv
